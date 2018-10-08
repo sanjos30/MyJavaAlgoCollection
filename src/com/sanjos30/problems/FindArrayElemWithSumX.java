a81feb130828eb95e80be01fe69b7f12a5c95c5d
SanjoS30 <sandeep.combat@gmail.com>
2018-10-08 23:02:00 +1100

    bug fixes

--- src/com/sanjos30/problems/FindArrayElemWithSumX.java
+++ src/com/sanjos30/problems/FindArrayElemWithSumX.java
@@ -1,5 +1,8 @@
 package com.sanjos30.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindArrayElemWithSumX {
 
 	public static void main(String[] args) {
@@ -14,7 +17,32 @@ public class FindArrayElemWithSumX {
 
 	private static void findElemWithSumK(int[] arr, int k) {
 
+	Map<Integer,Integer> hMap=new HashMap<Integer,Integer>();
 		
+		for(int i:arr) {
+			
+			if(!hMap.containsKey(i))
+				hMap.put(i, 1);
+			else {
+				int count=hMap.get(i);
+				hMap.put(i, ++count);				
+			}
+		}
+		
+		int count=0;
+		for(int i:arr) {
+			int temp=k-i;
+			
+			if(hMap.containsKey(temp)) {
+				
+				count++;
+				System.out.println(i + ", " + temp);
+			}
+				
+			
+		}
+		
+		System.out.println(count);
 	}
 
 }
