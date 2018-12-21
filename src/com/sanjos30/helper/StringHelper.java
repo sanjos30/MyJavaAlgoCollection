ed731af130fef0dbfd225b2c52bb37af8e5c1070
SanjoS30 <sandeep.combat@gmail.com>
2018-12-22 10:36:27 +1100

    some minor changes

--- src/com/sanjos30/helper/StringHelper.java
+++ src/com/sanjos30/helper/StringHelper.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class StringHelper {
 
 	public static void main(String args[]) {
-		System.out.println(isSubSequence("apples", "abplesc"));
+		System.out.println(isSubSequence("apple", "abplesc"));
 	}
 
 	/**
